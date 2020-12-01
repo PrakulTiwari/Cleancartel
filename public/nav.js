20232b3e9117cb474b65a647d8b6ae80ddeee775
Prakul <prakultiwari28@gmail.com>
2020-12-02 00:24:22 +0530

    'hell'

--- public/nav.js
+++ public/nav.js
@@ -3,11 +3,13 @@ const filling1 = document.querySelector('nav .logo-burger .burger .filling1');
 const filling2 = document.querySelector('nav .logo-burger .burger .filling2');
 const filling3 = document.querySelector('nav .logo-burger .burger .filling3');
 const menu = document.querySelector('nav .menu');
-const li = document.querySelector('nav .menu li a')
+const a = document.querySelector('nav .menu li .a')
+const b = document.querySelector('nav .menu li .b')
+const c = document.querySelector('nav .menu li .c')
 const logo = document.querySelector('nav .logo-burger li a')
 
 const open = () => {
-    burger.style.border = '3px solid rgb(255, 129, 150)';
+    menu.style.display = 'flex';
     filling1.style.background = 'rgb(255, 129, 150)';
     filling2.style.background = 'rgb(255, 129, 150)';
     filling3.style.background = 'rgb(255, 129, 150)';
@@ -20,7 +22,6 @@ const open = () => {
     menu.style.animation = 'menu 0.5s linear';
 }
 const close = () => {
-    burger.style.border = '3px solid #ffd700';
     filling1.style.background = '#ffd700';
     filling2.style.background = '#ffd700';
     filling3.style.background = '#ffd700';
@@ -31,7 +32,18 @@ const close = () => {
     filling2.style.opacity = '1';
     menu.style.animation = 'menuBack 0.5s linear';
     menu.style.opacity = '0';
-
+    setTimeout(() => menu.style.display = 'none', 500)
+}
+const menoi = () => {
+    a.addEventListener('click', () => {
+        close();
+    })
+    b.addEventListener('click', () => {
+        close();
+    })
+    c.addEventListener('click', () => {
+        close();
+    })
 }
 
 var clicks = 0;
@@ -47,9 +59,7 @@ burger.onmouseenter = function () {
 burger.addEventListener('click', () => {
     if (clicks % 2 != 0) {
         open();
-        li.addEventListener('click', () => {
-            close();
-        })
+        menoi()
         logo.addEventListener('click', () => {
             close();
         })
@@ -63,9 +73,7 @@ burger.addEventListener('click', () => {
 burger.addEventListener('mouseenter', () => {
     if (mouses % 2 != 0) {
         open();
-        li.addEventListener('click', () => {
-            close();
-        })
+        menoi()
         logo.addEventListener('click', () => {
             close();
         })
